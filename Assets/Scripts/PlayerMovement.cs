c710e9abc61283617ab4dd6a183ed2be58ee8184
Geta0745 <82598256+Geta0745@users.noreply.github.com>
2022-10-13 00:45:41 +0700

    Fixed Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Components")]
-    public Rigidbody tankRigidbody;
+    public Rigidbody rb;
     Animator animator;
     public PlayerControl PlayerControls;
     private InputAction move;
-    [SerializeField] float maxSpeed = 5f;
-    [SerializeField] float maxReverseSpeed = -3.5f;
-    [SerializeField] float bakeForce =  1f;
-    [SerializeField] float currentSpeed;
-    [SerializeField] float accelerate = .1f;
-    [SerializeField] float turnSpeed;
+    [SerializeField] float maxSpeed = 10f;
+    [SerializeField] float maxReverseSpeed = 5f;
+    [SerializeField] float accelerate = 5f;
+    [SerializeField] float turnSpeed = 45f;
+    [SerializeField] float currentTurnSpeed;
     [SerializeField]
     Vector2 movement;
 
@@ -34,11 +33,11 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Start() {
-        if(tankRigidbody == null){
-            tankRigidbody = gameObject.GetComponent<Rigidbody>();
+        if(rb == null){
+            rb = gameObject.GetComponent<Rigidbody>();
         }
         animator = gameObject.GetComponent<Animator>();
-        tankRigidbody.centerOfMass = new Vector3(0f,-0.05f,0f);
+        //rb.centerOfMass = new Vector3(0f,-0.05f,0f);
     }
 
     private void Update() {
@@ -51,21 +50,31 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void CalSpeed(){
-        if(movement.y > 0f){
-            currentSpeed = Mathf.Lerp(currentSpeed,maxSpeed * movement.y,accelerate * Mathf.Abs(movement.y));
-        }else if(movement.y < 0f){
-            currentSpeed = Mathf.Lerp(currentSpeed,maxReverseSpeed,accelerate * Mathf.Abs(movement.y));
-        }else{
-            currentSpeed = Mathf.Lerp(currentSpeed,0f,bakeForce);
+        if(movement.y > 0f && rb.velocity.magnitude < maxSpeed){
+            Debug.Log("forward");
+            rb.AddForce(rb.transform.forward * accelerate);
+        }else if(movement.y < 0f && rb.velocity.magnitude < maxReverseSpeed){
+            Debug.Log("backward");
+            rb.AddForce(-rb.transform.forward * accelerate);
         }
     }
 
     private void FixedUpdate() {
-        Vector3 movement_ = transform.forward * currentSpeed * Time.deltaTime;
-        tankRigidbody.MovePosition(tankRigidbody.position + movement_);
-        float turn = movement.x * turnSpeed  * Time.deltaTime;
-        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
-        tankRigidbody.MoveRotation(tankRigidbody.rotation * turnRotation);
+        Rotate();
     }
-    
+
+    void Rotate(){
+        CalTurnSpeed();
+        float turn = currentTurnSpeed;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+
+    void CalTurnSpeed(){
+        float absSpeed = 1f;
+        if(Mathf.Abs(movement.y) != 0f){
+            absSpeed = Mathf.Abs(movement.y);
+        }
+        currentTurnSpeed = movement.x * turnSpeed *  absSpeed * Time.deltaTime;
+    }
 }
