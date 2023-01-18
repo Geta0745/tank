6f891910a7760f3f76a125ed4c0b6f9d3f2bb795
Geta0745 <82598256+Geta0745@users.noreply.github.com>
2023-01-18 13:55:53 +0700

    Update TankComponent.cs

--- Assets/Scripts/New Player Script/TankComponent.cs
+++ Assets/Scripts/New Player Script/TankComponent.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TankComponent : MonoBehaviour
 {
   public float damageMultiply = 2f; //like 10f -2f = 2f
-  public float size = 1f;
+  public float radius = 1f;
   public float durability = 10f;
   public float maxdurability = 10f;
   public string name = "Some Component";
